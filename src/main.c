968dc2c20173db9b7074714976be919d88b17e6a
Fabio Mauri <fabio.mauri@gmail.com>
2015-12-29 00:25:04 +0100

    Added battery indicator

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static Layer *s_canvas_layer;
+static GFont s_time_font;
+static int s_battery_level;
 
 static void update_time() {
   // Get a tm structure
@@ -17,24 +20,65 @@ static void update_time() {
   text_layer_set_text(s_time_layer, s_buffer);
 }
 
+static void battery_handler(BatteryChargeState charge_state) {
+  s_battery_level = charge_state.charge_percent;
+  layer_mark_dirty(s_canvas_layer);
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+}
+
+static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+  // read bounds
+  GRect bounds = layer_get_bounds(this_layer);
+  
+  // modifying inset if needed
+  //GRect frame = grect_inset(bounds, GEdgeInsets(30));
+  
+  // battery indicator color
+  if (s_battery_level > 40)
+    graphics_context_set_fill_color(ctx, GColorGreen);
+  if ((s_battery_level <= 40) && (s_battery_level > 20))
+    graphics_context_set_fill_color(ctx, GColorYellow);
+  if (s_battery_level <= 20)
+    graphics_context_set_fill_color(ctx, GColorRed);
+  
+  // drawing battery indicator
+  graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, 15,
+                       DEG_TO_TRIGANGLE(0), 
+                       DEG_TO_TRIGANGLE((360*s_battery_level)/100));
 }
 
 static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  
+  // get battery level
+  BatteryChargeState charge_state = battery_state_service_peek();
+  s_battery_level = charge_state.charge_percent;
+  
+  // subscribe for battery change level notification
+  battery_state_service_subscribe(battery_handler);
+  
+  // Creating battery Layer
+  s_canvas_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  layer_add_child(window_layer, s_canvas_layer);
+  layer_set_update_proc(s_canvas_layer, canvas_update_proc);
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+  
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_INSOMNIA_REGULAR_48));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   //text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
@@ -44,6 +88,8 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
 }
 
 static void init() {
