4f1ec17270fb8646b0db90ffc244112a68c02f78
Fabio Mauri <fabio.mauri@gmail.com>
2016-01-02 18:28:55 +0100

    Improved time and date visualization
    - fixed date overlap for square devices
    - fixed 12h visualization (no leading 0)

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_bt_layer;
+static TextLayer *s_date_layer;
 static Layer *s_canvas_layer;
 static GFont s_time_font;
 static GFont s_bt_font;
@@ -15,11 +16,14 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
+  static char s_buffer_date[14];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
+                                          "%k:%M" : "%l:%M", tick_time);
+  strftime(s_buffer_date, sizeof(s_buffer_date), "%a\n%D", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(s_date_layer, s_buffer_date);
 }
 
 static void battery_handler(BatteryChargeState charge_state) {
@@ -38,6 +42,7 @@ static void bt_update_label(bool connected) {
   } else {
     text_layer_set_text_color(s_bt_layer, GColorRed);
     text_layer_set_text(s_bt_layer, "not connected");
+    vibes_long_pulse();
   }
 }
 
@@ -71,42 +76,50 @@ static void main_window_load(Window *window) {
   BatteryChargeState charge_state = battery_state_service_peek();
   s_battery_level = charge_state.charge_percent;
   
-  // Creating battery Layer
+  // BATTERY LEVEL DISPLAY layer
   s_canvas_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
   layer_add_child(window_layer, s_canvas_layer);
   layer_set_update_proc(s_canvas_layer, canvas_update_proc);
   layer_mark_dirty(s_canvas_layer);
-
-  // Create the TextLayer with specific bounds
+  
+  // CREATING FONTS for time
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_INSOMNIA_REGULAR_48));
+  // for btluetooth and date
+  s_bt_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_INSOMNIA_REGULAR_16));
+  
+  // TIME DISPLAY
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-  
-  // Create GFont for time
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_INSOMNIA_REGULAR_48));
-  // Create GFont for bt
-  s_bt_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_INSOMNIA_REGULAR_16));
-
-  // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   //text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   
-  // create text layer for bt connection feedback
+  // BLUETOOTH create text layer for bt connection feedback
   s_bt_layer = text_layer_create(
-      GRect(0, 120, bounds.size.w, 50));
+      GRect(0, 120, bounds.size.w, 25));
   text_layer_set_background_color(s_bt_layer, GColorClear);
   text_layer_set_text_color(s_bt_layer, GColorBlack);
+  // update text
   bt_update_label(connection_service_peek_pebble_app_connection());
   text_layer_set_font(s_bt_layer, s_bt_font);
   text_layer_set_text_alignment(s_bt_layer, GTextAlignmentCenter);
-  
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_bt_layer));
+  
+  // DATE create text layer
+  s_date_layer = text_layer_create(
+      GRect(0, 25, bounds.size.w, 50));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  // update text
+  text_layer_set_font(s_date_layer, s_bt_font);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));  
 }
 
 static void main_window_unload(Window *window) {
@@ -114,6 +127,8 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
   // Destroy bt TextLayer
   text_layer_destroy(s_bt_layer);
+  // Destroy date display layer
+  text_layer_destroy(s_date_layer);
   // Destroy canvas layer
   layer_destroy(s_canvas_layer);
   // Unload GFont
@@ -132,6 +147,10 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  
+  // Setting locale
+  setlocale(LC_ALL, "");
+  
   // Make sure the time is displayed from the start
   update_time();
   
